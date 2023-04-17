da3fdcd34ff04a30bc5b00bdf2601b0c18b06349
kajacx <kajacx@gmail.com>
2023-04-17 16:30:42 +0200

    Initialize a wasm-pack example

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+cd hello-wasmer-u64 && wasm-pack build --target web && \
+cd pkg && docker-compose -p wasmer-u64 down && docker-compose -p wasmer-u64 up -d && \
+cd ../.. && \
+echo "All done, view the webpage at http://127.0.0.1:8094"
